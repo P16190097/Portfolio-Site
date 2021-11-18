3595b0f7fbc4ee305580b06153efb77c6dd5a75b
mayoe <mayoze.jm@googlemail.com>
2021-11-18 21:42:56 +0000

    added fontawesome hero social icons

--- src/components/home/heroBanner.tsx
+++ src/components/home/heroBanner.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import useCanvas from '../../hooks/useCanvas';
 import drawHeroPlanet from '../../canvas/planet';
 
@@ -12,6 +15,11 @@ const HeroBanner = () => {
           Joshua Mayo
           <div className="hero-title-divider"></div>
           <div className="hero-subtitle">Web Developer</div>
+          <div className="hero-icons">
+            <a href="mailto:mayoe@live.co.uk" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a>
+            <a href="https://github.com/p16190097" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+            <a href="https://www.linkedin.com/in/joshua-mayo-3949a219b/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+          </div> 
         </div>
       </div>
       <canvas className="hero-canvas" ref={canvasRef} />
