a7836a49bad9bd684d7a4de4197782f215d6cffa
mayoe <mayoze.jm@googlemail.com>
2024-02-01 13:14:37 +0000

    removed hero icons

--- src/components/home/heroBanner.tsx
+++ src/components/home/heroBanner.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import useCanvas from '../../hooks/useCanvas';
 import drawHeroPlanet from '../../canvas/planet';
 import headshot from '../../images/headshot1.jpg';
@@ -17,11 +14,6 @@ const HeroBanner = () => {
           <div>Joshua Mayo</div>
           <div className="hero-title-divider"></div>
           <div className="hero-subtitle">Web Developer</div>
-          <div className="hero-icons">
-            <a href="mailto:mayoe@live.co.uk" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a>
-            <a href="https://github.com/p16190097" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="https://www.linkedin.com/in/joshua-mayo-3949a219b/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-          </div> 
         </div>
       </div>
       <canvas className="hero-canvas" ref={canvasRef} />
